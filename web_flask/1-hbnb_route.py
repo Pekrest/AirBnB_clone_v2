2a3abb7b2932e6fbda046b7e5cee2d7e3b4a541c
pekrest <peteroku3@gmail.com>
2024-05-19 07:21:25 -0700

    hbnb task

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+
+"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes=False
+
+@app.route("/")
+def home_hbnb():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+@app.route("/hbnb")
+def hbnb():
+    """Displays 'HBNB'."""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
